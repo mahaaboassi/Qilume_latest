fdfc33df0ab617c82c6cc650de6af3fa1a3b00b0
Maha Abo Assi <eng.mahaab96@gmail.com>
2026-01-05 18:23:51 +0400

    change the treatment section to categories

--- app/component/carousel/carousel.tsx
+++ app/component/carousel/carousel.tsx
@@ -1,5 +1,5 @@
 
-import { services } from "@/app/data/data"
+import { servicesRef } from "@/app/data/data"
 import {
   Carousel,
   CarouselContent,
@@ -22,12 +22,12 @@ export function CarouselSize() {
         <CarouselNext className="w-[30px] h-[30px] md:w-[50px] md:h-[50px]  !p-0 !rounded-full" />
       </div>
       <CarouselContent>
-        {services.map((item, index) => (
+        {servicesRef.map((item, index) => (
           <CarouselItem key={index} className="xs:basis-1/2 lg:basis-1/4">
             <div className="p-1 space-y-2">
                <Image className={`object-cover rounded-xl !w-full ${index%2 == 0 ? "!h-[280px] md:!h-[350px]":"!h-[230px] md:!h-[300px]"}`} width={400} height={100} src={item.img} alt={item.name } />
                 <h3 className="text-xl font-semibold">{item.name }</h3>
-                <p className="text-xs md:text-sm font-medium">{item.desc}</p>
+                <p className="text-xs md:text-sm font-medium">{item.description}</p>
             </div>
           </CarouselItem>
         ))}
