e88083d73f6d375ceeb6f14ae1185a0b778c4843
Maha Abo Assi <eng.mahaab96@gmail.com>
2026-01-06 15:15:25 +0400

    remove arrow from small size

--- app/component/navbar/navbar.tsx
+++ app/component/navbar/navbar.tsx
@@ -47,7 +47,7 @@ const Navbar = ()=>{
     if (closeTimeout.current) clearTimeout(closeTimeout.current);
     if (cat) setCurrentCat(cat);
     };
-    return(<nav className={`fixed left-0 right-0 top-0 bg-white  flex items-end justify-between z-100 layout-doc !py-1 md:!py-3 shadow-xl ${Style.nav}`}>
+    return(<nav className={`fixed left-0 right-0 top-0 bg-white flex items-end justify-between z-100 layout-doc !py-1 md:!py-3 shadow-xl ${Style.nav}`}>
         <div>
             <Link  onClick={()=>setOpenMenu(false)} href={"/"}>
                 <svg className={`${Style.logo}`} xmlns="http://www.w3.org/2000/svg" width="175" height="58" viewBox="0 0 175 58" fill="none">
@@ -112,11 +112,11 @@ const Navbar = ()=>{
                                             </svg>
                                         </div>
                                     </span>
-                                    <span className="flex md:hidden">
+                                    {/* <span className="flex md:hidden">
                                         <svg style={{transform:"rotate(180deg)"}} xmlns="http://www.w3.org/2000/svg" width="10" height="21" viewBox="0 0 29 40" fill="none">
                                         <path d="M29 4.7L11.0756 20L29 35.3L23.4818 40L-1.31887e-06 20L23.4818 3.33228e-06L29 4.7Z" fill="#72383D"/>
                                         </svg>
-                                    </span>
+                                    </span> */}
                                 </div>
                                 {/* Services Menu  */}
                                 { "name" in currentCat &&  <div className="fixed shadow-xl grid grid-cols-3 lg:flex lg:flex-col gap-2 lg:gap-10 left-0 right-0 layout-doc !py-[30px] top-[95px] md:top-[100px] lg:top-[70px]  z-100 bg-white">
