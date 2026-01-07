e88b8614adbf34173abea7817b9942eec52da305
Maha Abo Assi <eng.mahaab96@gmail.com>
2026-01-07 09:20:23 +0400

    change posiion of video to hero section

--- app/sections/hero/hero copy.tsx
+++ app/sections/hero/hero copy.tsx
@@ -1,3 +1,20 @@
+
+
+// "use client"
+// import styles from "./Hero.module.css"
+// const Hero = ()=>{
+//     return(<div className="!pt-15 md:!pt-20 ">
+//         <div className={`top-0 -z-5 w-full ${styles.videoContainer}`}>
+//             <video autoPlay loop muted className="w-full h-full object-cover">
+//                 <source src="/videos/qilume.mp4" type="video/mp4" />
+//             </video>
+//         </div>
+//         <div className={styles.overlay}>
+//         </div>
+//     </div>)
+// }
+// export default Hero
+
 "use client"
 import Image from "next/image"
 import styles from "./Hero.module.css"
